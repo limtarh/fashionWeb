cf056a17eb2afe362c892be6e53d90f2d36a4a53
limtarh <lim2001.ln@gmail.com>
2023-06-21 16:18:24 -0400

    DR. SD's code

--- pages/suggest.js
+++ pages/suggest.js
@@ -1,92 +1,58 @@
-"use client";
-import React, { useState } from "react";
-
 require('dotenv').config()
+
 const mysql = require('mysql2')
 
 
-function Outfit(){
-    return(
-        <>
-            <Top></Top>
-            <Bottom></Bottom>
-            <Shoes></Shoes>
-        </>
+function Suggestions() {
 
-    )
-}
-
-function Top(){
-    const [weather,setWeather] = useState("hot");
-    const i = weather == "hot"?"/images/summerT.jpeg":"/images/blackT.jpeg";
-
-    function toggleWeather(){
-        setWeather(weather => (weather === "hot" ? "cold" : "hot"));
-    }
-    
-    return(
-        <>
-            <h2>Top</h2>
-            <button type="button" onClick={toggleWeather}>Toggle</button>
-            <img src={i}/>
-        </>
-    )
-}
-
-function Bottom(){
-    const [weather,setWeather] = useState("hot");
-    const i = weather == "hot"?"/images/summerShorts.jpeg":"/images/sweatPants.jpeg";
-
-    function toggleWeather(){
-        setWeather(weather => (weather === "hot" ? "cold" : "hot"));
-    }
-    
-    return(
-        <>
-            <h2>Bottom</h2>
-            <button type="button" onClick={toggleWeather}>Toggle</button>
-            <img src={i}/>
-        </>
-    )
-}
-
-function Shoes(){
-    const [weather,setWeather] = useState("hot");
-    const i = weather == "hot"?"/images/nikeSlides.webp":"/images/toroBravo4s.jpeg";
-
-    function toggleWeather(){
-        setWeather(weather => (weather === "hot" ? "cold" : "hot"));
-    }
-    
-    return(
-        <>
-            <h2>Shoes</h2>
-            <button type="button" onClick={toggleWeather}>Toggle</button>
-            <img src={i}/>
-        </>
-    )
-}
-
-function Suggest(){
     const connection = mysql.createConnection(process.env.DATABASE_URL)
+
     console.log('Connected to PlanetScale!')
+
+
     connection.query(
-        "SELECT * FROM `inventory`",
-        function(err, results, fields) {
-          console.log(err);
-          console.log(results); // results contains rows returned by server
-          console.log(fields); // fields contains extra meta data about results, if available
-        }
-      );
+
+  "SELECT * FROM `inventory`",
+
+  function(err, results, fields) {
+
+    console.log(err);
+
+    console.log(results); // results contains rows returned by server
+
+    console.log(fields); // fields contains extra meta data about results, if available
+
+  }
+
+);
+
+
     connection.end()
-    return(
-        <>
-            <h1>Suggestions</h1>
-            <Outfit></Outfit>
 
-        </>
+    const numOutfits = 1;
 
-    )
+    const theOutfits = [...Array(numOutfits)].map((idx) => (<Outfit />));
+
+    return (<>
+        <main>
+        <title>suggestions</title>
+        <p>barbanera shirt</p>
+        {theOutfits}
+
+        </main>
+            </>);
+
 }
 
-export default Suggest;
+
+function Outfit() {
+
+    const theTop = Math.random() > 0.5 ? "/images/blackShoes.jpeg" : "/images/beigeShoes.jpeg" ;
+
+    return (<>
+        <img src={theTop} />
+        </>);
+
+}
+
+export default Suggestions ;
